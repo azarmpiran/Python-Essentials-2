b8c20be739ba63540eb14d3b8d0c3fddff13a46d
Azarm Piran <apiran.data@gmail.com>
2023-10-25 12:29:39 -0700

    update

---
+++ 02-21-2023/main.py
@@ -0,0 +1,14 @@
+# Azarm Piran
+# 02-21-2023
+
+
+# Python Essential 2 - Module 1
+
+import math as math
+
+# To access all the entities of math library you need to use the module name
+print(math.pi)
+
+pi = 2
+print(pi)
+print(math.pi)
